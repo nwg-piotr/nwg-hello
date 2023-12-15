b76702547bf17a92a488bf36ee0b3b84c8759da7
piotr <nwg.piotr@gmail.com>
2023-12-15 01:49:13 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 install -D -m 644 -t /etc/greetd/ nwg-hello-default.json
 install -D -m 644 -t /etc/greetd/ nwg-hello-default.css
 install -D -m 644 -t /usr/share/nwg-hello/ nwg.jpg
+install -D -m 644 -t /usr/share/nwg-hello/ img/*
 
 python3 setup.py install --optimize=1
