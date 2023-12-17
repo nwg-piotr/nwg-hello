1378444b8ef54cd8cf001b1aa358d76bbdcc9e15
piotr <nwg.piotr@gmail.com>
2023-12-17 01:46:08 +0100

    testing

--- nwg_hello/ui.py
+++ nwg_hello/ui.py
@@ -148,9 +148,9 @@ class GreeterWindow(Gtk.Window):
 
     def on_login_btn(self, btn):
         cmd = self.combo_session.get_active_id()
-        jreq = {"type": "start_session", "cmd": cmd.split()}
-        resp = greetd(self.client, jreq)
-        print("resp3", resp)
-        self.lbl_message.set_text("on_login_btn")
-        if "type" in resp and resp["type"] == "success":
-            sys.exit(0)
+        # jreq = {"type": "start_session", "cmd": cmd.split()}
+        # resp = greetd(self.client, jreq)
+        # print("resp3", resp)
+        self.lbl_message.set_text(cmd)
+        # if "type" in resp and resp["type"] == "success":
+        #     sys.exit(0)
