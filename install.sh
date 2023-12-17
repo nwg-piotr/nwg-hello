e695bfc3c4da0e101daf5e47be6fb2b29203f96c
piotr <nwg.piotr@gmail.com>
2023-12-17 14:33:14 +0100

    restore logging

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ install -D -m 644 -t /etc/nwg-hello/ README
 install -D -m 644 -t /usr/share/nwg-hello/ nwg.jpg
 install -D -m 644 -t /usr/share/nwg-hello/ img/*
 
+sudo mkdir -p /var/nwg-hello && sudo chown -R greeter:greeter /var/nwg-hello && sudo chmod -R 660 /var/nwg-hello
+
 python3 setup.py install --optimize=1
