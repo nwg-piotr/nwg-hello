f4eb684b3a0e6f84c6dbb38c6ed5c1e30ae8fd02
piotr <nwg.piotr@gmail.com>
2023-12-18 15:38:08 +0100

    fix cache installation

--- install.sh
+++ install.sh
@@ -9,6 +9,5 @@ install -D -m 644 -t /usr/share/nwg-hello/ img/*
 
 install -d /var/cache/nwg-hello
 install -Dm644 -t /var/cache/nwg-hello cache.json -o greeter
-#install -o greeter /var/cache/nwg-hello/cache.json
 
 python3 setup.py install --optimize=1
