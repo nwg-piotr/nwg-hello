e9e183ae32eefb5c1ba46c1bf2463f62718625cb
piotr <nwg.piotr@gmail.com>
2023-12-21 04:16:32 +0100

    adjust layout to hd screen

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(f_name):
 
 setup(
     name='nwg-hello',
-    version='0.1.0',
+    version='0.1.1',
     description='GTK3-based greeter for greetd',
     packages=find_packages(),
     include_package_data=True,
