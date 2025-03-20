e8162e7757df3f4753b7ab82ee33323bf999d9bc
piotr <nwg.piotr@gmail.com>
2025-03-20 01:40:51 +0100

    improve install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 
-# Make sure you have 'python-build' 'python-installer' 'python-wheel' and 'python-setuptools' installed
+# Before running this script, make sure you have python-build, python-installer,
+# python-wheel and python-setuptools installed.
+
+PROGRAM_NAME="nwg-hello"
+MODULE_NAME="nwg_hello"
+SITE_PACKAGES="$(python3 -c "import sysconfig; print(sysconfig.get_paths()['purelib'])")"
+PATTERN="$SITE_PACKAGES/$MODULE_NAME*"
+
+# Remove from site_packages
+for path in $PATTERN; do
+    if [ -e "$path" ]; then
+        echo "Removing $path"
+        rm -r "$path"
+    fi
+done
+
+[ -d "./dist" ] && rm -rf ./dist
+
+# Remove launcher scripts
+filenames=("/usr/bin/$PROGRAM_NAME")
+
+for filename in "${filenames[@]}"; do
+  rm -f "$filename"
+  echo "Removing -f $filename"
+done
+
+python -m build --wheel --no-isolation
+
+python -m installer dist/*.whl
 
 install -D -m 644 -t /etc/nwg-hello/ nwg-hello-default.json
 install -D -m 644 -t /etc/nwg-hello/ nwg-hello-default.css
@@ -13,9 +41,5 @@ install -D -m 644 -t /usr/share/nwg-hello/ img/*
 install -d /var/cache/nwg-hello
 install -Dm644 -t /var/cache/nwg-hello cache.json -o greeter
 
-install -Dm 644 -t "/usr/share/licenses/nwg-hello" LICENSE
-install -Dm 644 -t "/usr/share/doc/nwg-hello" README.md
-
-python -m build --wheel --no-isolation
-[ -f /usr/bin/nwg-hello ] && sudo rm /usr/bin/nwg-hello
-python -m installer dist/*.whl
+install -Dm 644 -t "/usr/share/licenses/$PROGRAM_NAME" LICENSE
+install -Dm 644 -t "/usr/share/doc/$PROGRAM_NAME" README.md
